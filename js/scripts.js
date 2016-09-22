59013ccb85a8d00a25beab18ecea6999e681376e
Marty Kovach <tyedye105@yahoo.com>
2016-09-22 13:47:00 -0700

    add back end logic that takes in two variables and outputs the coressponding vacation location.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,34 @@
+ var yourVacation = function( favoriteSeason, favoriteActivity) {
+   if (favoriteSeason === "carribean1" && favoriteActivity === "carribean2") {
+     $("#questionaire").toggle();
+     $("#hidden-carribean").toggle();
+     event.preventDefault();
+
+
+    } else if (favoriteSeason === "iceland1" && favoriteActivity === "iceland2") {
+      $("#questionaire").toggle();
+      $("#hidden-iceland").toggle();
+      alert("hi");
+      event.preventDefault();
+
+
+    } else if (favoriteSeason === "japan1" && favoriteActivity === "japan2") {
+      $("#questionaire").toggle();
+      $("#hidden-japan").toggle();
+      alert("hi");
+      event.preventDefault();
+    } else if (favoriteSeason === "newzealand1" && favoriteActivity === "newzealand2") {
+      $("#questionaire").toggle();
+      $("#hidden-newzealand").toggle();
+      alert("hi");
+      event.preventDefault();
+
+   }
+
+ }
+
+
+
 
 //Front End logic
 $(document).ready(function(event) {
@@ -7,8 +38,9 @@ $(document).ready(function(event) {
     var favoriteDrink = $("#drink").val();
     var ringResponse = $("#ring").val();
     var favoriteCompanion = $("input:radio[name=companion]:checked").val();
-      alert( favoriteSeason  +" "+ favoriteActivity +" "+ favoriteDrink +" "+ ringResponse +" "+ favoriteCompanion);
-      event.preventDefault();
+    var destination = yourVacation(favoriteSeason, favoriteActivity);
+
+
 
 
 
