a5f87a551224a2d0fa093c4b7188436613510e01
Marty Kovach <tyedye105@yahoo.com>
2016-09-23 13:20:15 -0700

    add logic to back and forth between results, survey form, and landing content.

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,110 @@
- var yourVacation = function( favoriteSeason, favoriteActivity) {
-   if (favoriteSeason === "carribean1" && favoriteActivity === "carribean2") {
+
+//Back-end logic
+
+// Location is detmerimned by questions 2,3, and 5.
+ var yourVacation = function( favoriteActivity, favoriteDrink, favoriteCompanion) {
+   if (favoriteActivity=== "carribean2" && favoriteDrink === "carribean3" && favoriteCompanion == "carribean5") {
      $("#questionaire").toggle();
      $("#hidden-carribean").toggle();
      event.preventDefault();
 
 
-    } else if (favoriteSeason === "iceland1" && favoriteActivity === "iceland2") {
+   } else if (favoriteActivity=== "iceland2" && favoriteDrink === "iceland3" && favoriteCompanion == "iceland5") {
       $("#questionaire").toggle();
       $("#hidden-iceland").toggle();
-      alert("hi");
       event.preventDefault();
 
-
-    } else if (favoriteSeason === "japan1" && favoriteActivity === "japan2") {
+    } else if (favoriteActivity === "japan2" && favoriteDrink === "japan3" && favoriteCompanion == "japan5") {
       $("#questionaire").toggle();
       $("#hidden-japan").toggle();
-      alert("hi");
       event.preventDefault();
-    } else if (favoriteSeason === "newzealand1" && favoriteActivity === "newzealand2") {
+
+    } else if (favoriteActivity === "newzealand2" && favoriteDrink === "newzealand3" && favoriteCompanion == "newzealand5") {
       $("#questionaire").toggle();
       $("#hidden-newzealand").toggle();
-      alert("hi");
       event.preventDefault();
+    } else {
+      singleChoice(favoriteCompanion);
+    }
 
-   }
 
  }
-
- var goLanding = function( favoriteSeason, favoriteActivity) {
-   if (favoriteSeason === "carribean1" && favoriteActivity === "carribean2") {
-     $("#hidden-carribean").hide();
-     $("#landing").show();
-     alert("done");
+ // incase survey don't match up to produce a result, this function is called and will determine the result based of question 5.
+ var singleChoice = function(favoriteCompanion) {
+   if (favoriteCompanion === "carribean5") {
+     $("#questionaire").toggle();
+     $("#hidden-carribean").toggle();
+     event.preventDefault();
+   } else if (favoriteCompanion === "iceland5") {
+     $("#questionaire").toggle();
+     $("#hidden-iceland").toggle();
+     event.preventDefault();
+   } else if (favoriteCompanion === "japan5") {
+      $("#questionaire").toggle();
+      $("#hidden-japan").toggle();
+      event.preventDefault();
+   } else if (favoriteCompanion === "newzealand5") {
+     $("#questionaire").toggle();
+     $("#hidden-newzealand").toggle();
      event.preventDefault();
 
+   }
+ }
+// Takes user back to the "landing" content
+ var goLanding = function( favoriteCompanion) {
+   if (favoriteCompanion === "carribean5") {
+     $("#hidden-carribean").toggle();
 
-    } else if (favoriteSeason === "iceland1" && favoriteActivity === "iceland2") {
-      $("#questionaire").toggle();
+     $("#landing").toggle();
+   } else if (favoriteCompanion === "iceland5") {
       $("#hidden-iceland").toggle();
-      alert("hi");
-      event.preventDefault();
+      $("#landing").toggle();
 
 
-    } else if (favoriteSeason === "japan1" && favoriteActivity === "japan2") {
-      $("#questionaire").toggle();
+
+
+    } else if (favoriteCompanion === "japan5") {
       $("#hidden-japan").toggle();
-      alert("hi");
-      event.preventDefault();
-    } else if (favoriteSeason === "newzealand1" && favoriteActivity === "newzealand2") {
-      $("#questionaire").toggle();
+      $("#landing").toggle();
+
+
+
+
+    } else if (favoriteCompanion === "newzealand5") {
       $("#hidden-newzealand").toggle();
-      alert("hi");
-      event.preventDefault();
+      $("#landing").toggle();
+
+
 
    }
 
  }
+ // this function will take them back to the survey form.
+var goSurvey = function(favoriteCompanion) {
+  if (favoriteCompanion === "carribean5") {
+    $("#hidden-carribean").toggle();
+    $("#questionaire").toggle();
 
+
+  } else if (favoriteCompanion === "iceland5") {
+     $("#hidden-iceland").toggle();
+     $("#questionaire").toggle();
+
+
+   } else if (favoriteCompanion === "japan5") {
+     $("#hidden-japan").toggle();
+     $("#questionaire").toggle();
+
+
+   } else if (favoriteCompanion === "newzealand5") {
+     $("#hidden-newzealand").toggle();
+     $("#questionaire").toggle();
+
+
+  }
+
+
+}
 
 
 
@@ -68,20 +116,29 @@ $(document).ready(function(event) {
     var favoriteDrink = $("#drink").val();
     var ringResponse = $("#ring").val();
     var favoriteCompanion = $("input:radio[name=companion]:checked").val();
-    var destination = yourVacation(favoriteSeason, favoriteActivity);
-    $("button.to-landing").click(function(event) {
-      alert(favoriteSeason);
-      goLanding (favoriteSeason, favoriteActivity);
-      event.preventDefault();
-    });
+    var destination = yourVacation(favoriteActivity, favoriteDrink, favoriteCompanion);
+});
 
 
-  });
   $("button#begin").click(function(event) {
     $("#landing").hide();
     $("#questionaire").show();
     event.preventDefault();
   });
+  $("button.to-landing").click(function(event) {
+    var favoriteCompanion = $("input:radio[name=companion]:checked").val();
+    goLanding (favoriteCompanion);
+    event.preventDefault();
+  });
+  $("button.to-survey").click(function(event) {
+    var favoriteCompanion = $("input:radio[name=companion]:checked").val();
+    goSurvey (favoriteCompanion);
+    event.preventDefault();
+  });
+
+
+
+});
+
 
 //endingtag
-});
